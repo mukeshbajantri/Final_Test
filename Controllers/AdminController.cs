e683a483382be2aef6e513765426c9d2c9689fed
mukeshbajantri <124239266+mukeshbajantri@users.noreply.github.com>
2023-02-02 14:40:03 +0530

    Add files via upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,185 @@
+﻿
+using System.Text;
+using System.Text.Json;
+using CarRentals.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CarRental_UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            IEnumerable<CarDetail> carDetailList = null;
+
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7161/api/");
+
+                var responseTask = client.GetAsync("CarDetails");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<CarDetail>>();
+                    readTask.Wait();
+
+                    carDetailList = readTask.Result;
+                }
+                else
+                {
+                    carDetailList = Enumerable.Empty<CarDetail>();
+
+                    ModelState.AddModelError(string.Empty, "Server error..");
+                }
+            }
+            return View(carDetailList);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int CarId)
+        {
+            using (var client = new HttpClient())
+            {
+                CarDetail carDetail = new CarDetail();
+                client.BaseAddress = new Uri("https://localhost:7161/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("CarDetails/" + CarId);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<CarDetail>();
+                    readTask.Wait();
+                    carDetail = readTask.Result;
+
+                    if (carDetail != null)
+                    {                       
+                        return PartialView("Edit", carDetail);
+                    }
+                    else
+                    {
+                        return PartialView("Error");
+                    }
+                }
+                else
+                {
+                    return PartialView("Error");
+                }
+            }
+            
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult DeleteCarData(int CarId)
+        {
+            using (var client = new HttpClient())
+            {
+                CarDetail carDetail = new CarDetail();
+                client.BaseAddress = new Uri("https://localhost:7161/api/");
+                //HTTP GET
+                var responseTask = client.DeleteAsync("CarDetails/" + CarId);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View("Error");
+        }
+
+        public IActionResult Delete(int CarId)
+        {
+            using (var client = new HttpClient())
+            {
+                CarDetail carDetail = new CarDetail();
+                client.BaseAddress = new Uri("https://localhost:7161/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("CarDetails/" + CarId);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<CarDetail>();
+                    readTask.Wait();
+                    carDetail = readTask.Result;
+
+                    if (carDetail != null)
+                    {
+                        return PartialView("Delete", carDetail);
+                    }
+                    else
+                    {
+                        return PartialView("Error");
+                    }
+                }
+                else
+                {
+                    return PartialView("Error");
+                }
+            }
+        }
+        public IActionResult SaveCreate(CarDetail car)
+            {
+            HttpClientHandler handler = new HttpClientHandler();
+            if (car!= null)
+            {
+                using (var client = new HttpClient(handler,false))
+                {
+
+                    CarDetail carDetail = new CarDetail();
+                    client.BaseAddress = new Uri("https://localhost:7161/api/");
+                    
+                    var carUpdate = JsonSerializer.Serialize<CarDetail>(car);
+                    var responseTask = client.PostAsync("CarDetails", new StringContent(carUpdate, Encoding.UTF8, "application/json"));
+                    responseTask.Wait();
+
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult UpdateCarDetails(CarDetail car)
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            IEnumerable<CarDetail> carDetails = null;
+            if (car != null)
+            {
+                using (var client = new HttpClient(handler,false))
+                {
+                    
+                    CarDetail carDetail = new CarDetail();
+                    client.BaseAddress = new Uri("https://localhost:7161/api/");
+                    //HTTP GET
+                    var carUpdate = JsonSerializer.Serialize<CarDetail>(car);
+                    var responseTask = client.PutAsync("CarDetails/"+car.CarId , new StringContent(carUpdate, Encoding.UTF8, "application/json"));
+                    responseTask.Wait();
+
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return PartialView("Error");
+        }
+    }
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,95 @@
+﻿using CarRentals.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Text.Json;
+
+namespace CarRental_UI.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index(string str)
+        {
+            if(str == null)
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else if(str.Equals("register"))
+            {
+                return View("Register");
+            }
+            else
+            {
+                return View("Login");
+            }
+            
+        }
+        public IActionResult Register(UserDetail user)
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7161/api/");
+                var userUpdate = JsonSerializer.Serialize<UserDetail>(user);
+                var responseTask = client.PostAsync("UserDetails", new StringContent(userUpdate, Encoding.UTF8, "application/json"));
+
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    this.TempData["Login"] = true;
+                    this.TempData["email"] = user.Email;
+                    
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                   return PartialView("Error");  
+                }
+            }
+        }
+
+        public IActionResult Login(UserDetail user)
+        {
+            if(user.Email == null || user.Password == null)
+            {
+                return PartialView("Error");
+            }
+            using (var client = new HttpClient())
+            {
+                UserDetail userDetail = new UserDetail();
+                client.BaseAddress = new Uri("https://localhost:7161/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("UserDetails/"+user.Email);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<UserDetail>();
+                    readTask.Wait();
+                    userDetail = readTask.Result;
+
+                    if(userDetail!=null && userDetail.Password.Equals(user.Password))
+                    {
+                        this.TempData["Login"] = true;
+                        this.TempData["Admin"] = userDetail.Admin;
+                        this.TempData["email"] = user.Email;
+                        
+                        return RedirectToAction("Index", "Dashboard");
+                    }
+                    else
+                    {
+                        return PartialView("Error");
+                    }
+                }
+                else
+                {
+                    return PartialView("Error");
+                }
+            }
+        }
+    }
+}
